0dbb85d01df4c6cb0b7e601671c3e01f24f1499d
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2022-08-12 02:41:35 -0400

    I hope this works..

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1700,7 +1700,7 @@ class PlayState extends MusicBeatState
 					schoolIntro(doof);
 				case 'bad-to-the-bone':
 					var video:MP4Handler = new MP4Handler();
-					video.playVideo(Paths.video('intro'));
+					video.playVideo(SUtil.getPath() + Paths.video('intro'));
 					video.finishCallback = function() 
 					{
 						papyintro(doof);
@@ -4993,7 +4993,7 @@ class PlayState extends MusicBeatState
 							//if(!ClientPrefs.getGameplaySetting('practice', false) && !ClientPrefs.getGameplaySetting('botplay', false))
 								//FreeplayState.songUnlock[2] = true;
 							var video:MP4Handler = new MP4Handler();
-							video.playVideo(Paths.video('final'));
+							video.playVideo(SUtil.getPath() + Paths.video('final'));
 							video.finishCallback = function() {
 								WeekData.loadTheFirstEnabledMod();
 								FlxG.sound.playMusic(Paths.music('freakyMenu'));
